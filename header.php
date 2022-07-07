f17fa0170015d54bae14fc983b28a5b533cba7a0
nomanson <jan@ingemansen.dk>
2022-07-07 12:25:20 +0200

    removed noindex

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="robots" content="noindex">
-
     <link rel="stylesheet" type="text/css" href="stylesheets/main.css">
 
     <title><?php echo $title; ?></title>
